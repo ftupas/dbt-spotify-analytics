18bb78a755fc2e3cb44a804bc2d0c22ae45573b0
Fred Tupas <fred.tupas@obf.ateneo.edu>
2022-02-03 00:37:33 +0100

    Modify docker files

--- dbt/dbt_entrypoint.sh
+++ dbt/dbt_entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-dbt init spotify_analytics &&
-cp -r /dbt_init/. /dbt/spotify_analytics &&
-cp -r /data/. /dbt/spotify_analytics/data &&
-rm -rf /dbt/spotify_analytics/models/example &&
-mv /dbt/spotify_analytics/profiles.yml /root/.dbt/profiles.yml
-cd /dbt/spotify_analytics &&
-dbt debug &&
-dbt deps &&
-dbt seed &&
-dbt run &&
-dbt test &&
-dbt docs generate &&
-dbt docs serve
+cp -r /data/. /dbt/data &&
+dbt debug --profiles-dir . &&
+dbt seed --profiles-dir . &&
+dbt deps --profiles-dir . &&
+dbt run --profiles-dir . &&
+dbt test --profiles-dir .  &&
+dbt docs generate --profiles-dir . &&
+dbt docs serve --profiles-dir .
